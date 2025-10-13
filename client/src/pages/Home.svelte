35634c2320c6125a193e0c21dfbf291820aa07a4
Glitch752 <xxGlitch752xx@gmail.com>
2025-10-13 01:16:16 -0500

    Add version info to home page

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,8 +1,19 @@
 <!-- TODO: use username lol -->
-<p>Welcome back, <span class="accent">user</span>!</p>
+<main>
+    <p>Welcome back, <span class="accent">user</span>!</p>
+    
+    <p>Version: {__APP_VERSION__}</p>
+    <p>Git SHA: {__GIT_COMMIT_HASH__}</p>
+</main>
 
 <style>
+main {
+    padding: 1rem;
+}
+p {
+    margin: 0.5rem 0;
+}
 .accent {
-    color: var(--blue);
+    color: var(--blue-text);
 }
 </style>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { execSync } from 'child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString();
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svelte(), devtoolsJson()],
+    
+    define: {
+        '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+        '__GIT_COMMIT_HASH__': JSON.stringify(commitHash.trim())
+    },
 
     server: {
         proxy: {
